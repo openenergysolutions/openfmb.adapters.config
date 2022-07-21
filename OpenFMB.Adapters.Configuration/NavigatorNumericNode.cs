1d1ad12a67c124169a7c9bd98e9845c7f4d44d95
Cory Nguyen <43071770+corynguyenfl@users.noreply.github.com>
2022-07-21 12:25:04 -0400

    Fixed scaling factor decimal places

--- OpenFMB.Adapters.Configuration/NavigatorNumericNode.cs
+++ OpenFMB.Adapters.Configuration/NavigatorNumericNode.cs
@@ -56,9 +56,8 @@ namespace OpenFMB.Adapters.Configuration
         }
 
         private void ValueControl_ValueChanged(object sender, EventArgs e)
-        {
-            var val = (Data.Tag as JProperty).Value as JValue;
-            if (val.Type == JTokenType.Integer)
+        {            
+            if (Data.Schema?.Type == Newtonsoft.Json.Schema.JSchemaType.Integer)
             {
                 (Data.Tag as JProperty).Value = new JValue((int)valueControl.Value);
             }
